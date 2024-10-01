e7945b9dcc31f0183b37bd1e33a438d7a558ce60
sergiosaraivajr <sergiosaraivajr01@gmail.com>
2024-10-01 02:14:47 -0300

    commit inicial

---
+++ background.js
@@ -0,0 +1,12 @@
+ // Alterar o background ao clicar nos botões
+document.getElementById('bg1').addEventListener('click', function() {
+    document.body.style.background = 'linear-gradient(to right, #000000, #434343)';
+});
+
+document.getElementById('bg2').addEventListener('click', function() {
+    document.body.style.background = 'linear-gradient(to right, #000428, #004e92)';
+});
+
+document.getElementById('bg3').addEventListener('click', function() {
+    document.body.style.background = 'linear-gradient(to right, #304352, #d7d2cc)';
+});
